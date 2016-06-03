4f386d5050f1760f4dd20ccd14238e3ae9079cb1
Matt <Matt@192.168.1.36>
2016-06-03 13:59:59 +0200

    Server lavet, connection tråd lavet, Engine.java er til test af server :)

--- 34318/src/server/Connection.java
+++ 34318/src/server/Connection.java
@@ -1,5 +1,53 @@
 package server;
 
-public class Connection {
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class Connection extends Thread {
+	
+	private ObjectInputStream input;
+	private ObjectOutputStream output;
+	
+	private boolean alive = true;
+	
+	private String clientIP = "";
+	private Socket client;
+
+	public Connection(Socket connection) {
+		this.client = connection;
+		clientIP = client.getRemoteSocketAddress().toString();
+	}
+	
+	@Override
+	public void run() {
+		super.run();
+		try {
+			configureStreams();
+		} catch(IOException e) {
+			e.printStackTrace();
+		} finally {
+			cleanUp();
+		}
+	}
+
+	private void configureStreams() throws IOException {
+		output = new ObjectOutputStream(client.getOutputStream());
+		output.flush();
+		input = new ObjectInputStream(client.getInputStream());
+	}
+
+	private void cleanUp() {
+		try {
+			input.close();
+			output.close();
+			client.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
 
 }
--- 34318/src/server/Server.java
+++ 34318/src/server/Server.java
@@ -1,5 +1,57 @@
 package server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+	
+	private ServerSocket server;
+	private Socket connection;
+
+	private boolean running = false;
+
+	public Server(int port) {
+		try {
+			server = new ServerSocket(port);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		sleepMode();
+	}
+
+	private void sleepMode() {
+		try {
+			waitForConnection();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			cleanUp();
+			System.exit(0);
+		}
+	}
+
+	private void waitForConnection() throws IOException {
+		while(running) {
+			connection = server.accept();
+			Connection newClient = new Connection(connection);
+			newClient.start();
+			try {
+				Thread.sleep(150);
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void cleanUp() {
+		try {
+			connection.close();
+			server.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
