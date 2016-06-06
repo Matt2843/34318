5fdd3c603cbf9f400b972dd4a7f5e0d5c98e49eb
chris <chris@192.168.0.15>
2016-06-06 14:13:13 +0200

    en gang til med penis

---
+++ 34318/src/client/ClientFTPUpload.java
@@ -0,0 +1,58 @@
+package client;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+ 
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+ 
+
+public class ClientFTPUpload {
+ 
+    public static void main(String[] args) {
+        String server = "www.myserver.com";
+        int port = 21;
+        String user = "user";
+        String pass = "pass";
+ 
+        FTPClient ftpClient = new FTPClient();
+        try {
+ 
+            ftpClient.connect(server, port);
+            ftpClient.login(user, pass);
+            ftpClient.enterLocalPassiveMode();
+ 
+            ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+ 
+            File firstLocalFile = new File("D:/Test/Projects.zip");
+ 
+            String firstRemoteFile = "Projects.zip";
+            InputStream inputStream = new FileInputStream(firstLocalFile);
+ 
+            System.out.println("Start uploading file");
+            boolean done = ftpClient.storeFile(firstRemoteFile, inputStream);
+            inputStream.close();
+            if (done) {
+                System.out.println("The file is uploaded successfully.");
+            }
+ 
+ 
+        } catch (IOException ex) {
+            System.out.println("Error: " + ex.getMessage());
+            ex.printStackTrace();
+        } finally {
+            try {
+                if (ftpClient.isConnected()) {
+                    ftpClient.logout();
+                    ftpClient.disconnect();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+ 
+}
