55979ee2756980b5e7b0ef478d336482cc06160d
s134516 <lise_noerby@hotmail.com>
2016-06-06 03:40:39 +0200

    Mere mainframe. Virker ikke endnu

--- 34318/src/gui/MainFrame.java
+++ 34318/src/gui/MainFrame.java
@@ -3,19 +3,26 @@ package gui;
 import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 
-public class MainFrame extends JPanel implements GeneralProperties {
+public class MainFrame extends JFrame implements GeneralProperties {
 	private DialogLogin DLogin;
 	private GridBagConstraints c = new GridBagConstraints();
+	private JPanel panel = new JPanel(new GridBagLayout()), JPLeft, JPRight;
+	
 	public MainFrame(){
 		DLogin = new DialogLogin(this);
 		DLogin.setAlwaysOnTop(true);
 	    DLogin.setVisible(true);
+	    setDefaultProperties();
+	    JPLeft = new PanelLeft(this);
+	    JPRight = new PanelRight(this);
+	    /*addC(JPLeft,0,0,1,1);
+	    addC(JPRight,1,0,1,1);
+	    mainFrameSetVisible();*/
 	}
 	
 	private void addC(JComponent comp, int x, int y, int height, int width) {
@@ -26,11 +33,9 @@ public class MainFrame extends JPanel implements GeneralProperties {
 		this.add(comp, c);
 	}
 	
-	private void setDefaultProperties(){
-		/*this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+	private void setDefaultProperties(){	
 		this.setUndecorated(true);
-		this.getContentPane().setBackground(Color.white);*/
-		this.setLayout(new GridBagLayout());
+		this.getContentPane().setBackground(Color.white);
 		this.setPreferredSize(GeneralProperties.frameSize);
 	}
 	
---
+++ 34318/src/gui/PanelRight.java
@@ -0,0 +1,53 @@
+package gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JPanel;
+
+public class PanelRight extends JPanel implements ActionListener, MouseListener {
+	
+	
+	public PanelRight(MainFrame parent){
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
