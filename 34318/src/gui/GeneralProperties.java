9494e18c12fcba644b48d8d989bb7ec7e4322e90
Lise <Lise@192.168.1.39>
2016-06-08 16:44:00 +0200

    Til mig selv

--- 34318/src/gui/GeneralProperties.java
+++ 34318/src/gui/GeneralProperties.java
@@ -5,8 +5,8 @@ import java.awt.Toolkit;
 public interface GeneralProperties {
 
 	public static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-	public static Dimension frameSize = new Dimension((screenSize.width / 3)*2, (screenSize.height / 4)*3);
-	public static Dimension panelLeftSize = new Dimension((frameSize.width/3), (frameSize.height));
+	public static Dimension frameSize = new Dimension((screenSize.width / 5)*4, (screenSize.height / 4)*3);
+	public static Dimension panelLeftSize = new Dimension((frameSize.width/5), (frameSize.height));
 	public static Dimension panelRightSize = new Dimension((frameSize.width/3)*2, (frameSize.height));
 	public static Dimension panelRightTopSize = new Dimension(panelRightSize.width, panelRightSize.height-80);
 	public static Dimension panelRightBottomSize = new Dimension(panelRightSize.width, 80);
