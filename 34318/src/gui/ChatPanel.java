b23921dce78dc7f48af1ffe171acc10fe2f9129c
Lise <Lise@192.168.1.39>
2016-06-09 20:47:54 +0200

    med panels

---
+++ 34318/src/gui/ChatPanel.java
@@ -0,0 +1,125 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
+
+public class ChatPanel extends JTabbedPane {
+	private MainFrame parent;
+	private JPanel ChatTab, JPUsers;
+	private JLabel JLClose;
+	private JTextArea JTUsersInChat;
+	private String[] Chats = {"Chat 1", "Chat 2", "Chat 3", "Chat 4"};
+	private String[] UsersInChat = {"User1", "User ", "User3","User 4"};
+	private int tabCounter = 0;
+	
+	
+	public ChatPanel(MainFrame parent){
+		this.parent = parent;
+		setDefaultProperties();
+		setComponents();
+	}
+	
+	private void setDefaultProperties(){
+		this.setPreferredSize(GeneralProperties.panelRightTopSize);
+		this.setBackground(Color.white);
+		this.setVisible(true);
+		this.validate();
+		
+	}
+	
+	private void setComponents(){
+		
+		
+		for (int i = 0; i < Chats.length; i++){
+			addTab(Chats[i]);
+		}
+	}
+	
+	public void addTab(String name){
+		final JPanel content = new JPanel(new BorderLayout());
+		makeUsersTab(name);
+		JTextArea JTTextChat = new JTextArea();
+		JTTextChat.setLineWrap(true); JTTextChat.setEditable(false);
+		JTTextChat.setText(name);
+		content.add(JTTextChat,BorderLayout.CENTER);
+		content.add(JPUsers,BorderLayout.EAST);
+		JLClose = new JLabel(parent.IClose);		
+		JLClose.addMouseListener(new MouseListener() {
+		      
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				int closeTabNumber = indexOfComponent(content);
+		        removeTabAt(closeTabNumber);
+				
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		    });
+		
+		ChatTab = new JPanel(new BorderLayout());
+		ChatTab.setOpaque(false);
+		ChatTab.add(new JLabel("Chat" + (++tabCounter) + "    "),BorderLayout.WEST);
+		ChatTab.add(JLClose,BorderLayout.EAST);
+		addTab(null, content);
+		setTabComponentAt(getTabCount() - 1, ChatTab);
+	}
+	
+	public JPanel makeUsersTab(String ID){
+		JPUsers = new JPanel(new BorderLayout());
+		JPUsers.setPreferredSize(GeneralProperties.panelUsersSize);
+		JPUsers.setBorder(BorderFactory.createLineBorder(Color.black));
+		JPUsers.setBackground(Color.WHITE);
+		
+		JTUsersInChat = new JTextArea();
+		for (int i = 0; i<UsersInChat.length;i++){
+			JTUsersInChat.append("\n   " + UsersInChat[i] );
+		}
+		JPanel JUsersIcons = new JPanel(new GridLayout(1,3));
+		JUsersIcons.setOpaque(false);
+		JUsersIcons.add(new JLabel(parent.IAdd));
+		JUsersIcons.add(new JLabel(parent.IBlock)); JUsersIcons.add(new JLabel());
+		JPanel JPUsersTop = new JPanel(new BorderLayout());
+		JLabel JLUsers = new JLabel("  Users");
+		JLUsers.setFont(new Font("SansSerif", Font.BOLD, 14));
+		JPUsersTop.add(JLUsers,BorderLayout.WEST);
+		JPUsersTop.add(new JLabel(),BorderLayout.CENTER);
+		JPUsersTop.add(JUsersIcons,BorderLayout.EAST);
+		JPUsersTop.setBackground(Color.WHITE);	
+		JPUsers.add(JPUsersTop, BorderLayout.NORTH);
+		JPUsers.add(JTUsersInChat, BorderLayout.CENTER);
+		return JPUsers;
+	}
+
+}
---
+++ 34318/src/gui/MessagePanel.java
@@ -0,0 +1,188 @@
+/*package gui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+public class MessagePanel extends JPanel implements MouseListener, KeyListener{
+	private MainFrame parent;
+	private JPanel  Menu, JPPictures, JPSmiley,JPFile;
+	private JLabel JLSmiley, JLFile;
+	private JButton JBSend;
+	private JTextArea JTText;
+	private JScrollPane scrollPane;
+	private String id = "Username";
+	
+	private ChatPanel chatPanel = new ChatPanel(parent);
+	
+	public MessagePanel(MainFrame parent){
+		this.parent = parent;
+		setDefaultProperties();
+		setComponents();
+		makeBottomPanel();
+	}
+	
+	private void setDefaultProperties(){
+		this.setLayout(new BorderLayout());
+		this.setBackground(Color.white);
+		this.setVisible(true);
+		this.validate();
+	}
+	
+	private void setComponents(){
+
+		Menu = new JPanel(new BorderLayout());
+		Menu.setBackground(Color.white);
+		
+
+		JPPictures = new JPanel(new GridLayout(1,2));
+		JPPictures.setBackground(Color.WHITE);
+		
+		JPSmiley = new JPanel();		
+		JPFile = new JPanel();
+				
+		JLSmiley = new JLabel(parent.ISmiley);
+		JLSmiley.setOpaque(false);
+		
+		JLFile = new JLabel(parent.IFile);
+		JLFile.setOpaque(false);
+		
+		JBSend = new JButton("Send");
+		JBSend.setFont(new Font("SansSerif", Font.PLAIN, 14));
+		JBSend.setBackground(Color.WHITE);
+		JBSend.addMouseListener(this);
+		
+		JTText = new JTextArea();
+		JTText.addKeyListener(this);
+		JTText.setLineWrap(true);
+		scrollPane = new JScrollPane(JTText);
+	}
+	
+	private void makeBottomPanel(){
+		JPPictures.add(JLSmiley);
+		JPPictures.add(JLFile);
+		Menu.add(JPPictures, BorderLayout.WEST);
+		Menu.add(new JLabel(),BorderLayout.CENTER);
+		this.add(Menu, BorderLayout.NORTH);
+		this.add(scrollPane,BorderLayout.CENTER);
+		this.add(JBSend, BorderLayout.EAST);
+		
+	}
+	
+	private void sendText(){
+		String message = JTText.getText();
+		System.out.println(message);
+		JTText.setText(null);
+		int selectedTab = chatPanel.getSelectedIndex();
+		JPanel JPSelectedPanel = (JPanel) TabbedPanel.getComponentAt(selectedTab);
+		JPSelectedPanel.setLayout(new GridLayout(30,1));
+		JPSelectedPanel.add(new JLabel(id + ":   " + message));
+		TabbedPanel.setComponentAt(selectedTab, JPSelectedPanel);
+		JPanel panel = new JPanel(new BorderLayout());
+		JTextArea chat = new JTextArea(id + ":   " + message); chat.setEditable(false); chat.setLineWrap(true);
+		panel.add(chat,BorderLayout.CENTER);
+		panel.add(chatPanel.makeUsersTab("Indsæt ID"),BorderLayout.EAST);
+		chatPanel.setComponentAt(selectedTab,panel);
+		Component editTab = chatPanel.getComponentAt(selectedTab);
+		editTab.addMouseListener(new MouseListener() {
+		      
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				int closeTabNumber = chatPanel.indexOfComponent(panel);
+		        chatPanel.removeTabAt(closeTabNumber);
+				
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		    });
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if (e.getSource() == JBSend){
+			sendText();
+		}
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == e.VK_ENTER){			
+			if (!JTText.getText().trim().equals("")){
+				sendText();
+			}
+			
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
+*/
