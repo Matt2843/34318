62d5f88749194f07ce868d4c481add63faad4585
Matt <Matt@192.168.1.36>
2016-06-09 21:40:35 +0200

    ChatTab class created.

---
+++ 34318/src/gui/ChatTab.java
@@ -0,0 +1,52 @@
+package gui;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+
+public class ChatTab extends JTextArea implements MouseListener {
+	private static final long serialVersionUID = 1L;
+	
+	private JLabel icon;
+	private JLabel name;
+	
+	public ChatTab(String tabName) {
+		name = new JLabel(tabName);
+		// icon
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+}
