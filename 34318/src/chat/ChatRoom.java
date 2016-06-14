908f3287b2e3f04d0c875a528ca7f8a72f127b35
chris <chris@192.168.1.64>
2016-06-14 14:11:22 +0200

    Moderators and admins added to ChatRoom

--- 34318/src/chat/ChatRoom.java
+++ 34318/src/chat/ChatRoom.java
@@ -9,6 +9,37 @@ public class ChatRoom implements Serializable {
 	private String chatName = null;
 	private String chatHistory = null;
 	private ArrayList<String> chatUsers = null;
+	private ArrayList<String> chatModeratiors = null;
+	private ArrayList<String> chatAdmins = null;
+	
+	
+	public String getChatName() {
+		return chatName;
+	}
+	public void setChatName(String chatName) {
+		this.chatName = chatName;
+	}
+	public ArrayList<String> getChatUsers() {
+		return chatUsers;
+	}
+	public void setChatUsers(ArrayList<String> chatUsers) {
+		this.chatUsers = chatUsers;
+	}
+	public ArrayList<String> getChatModeratiors() {
+		return chatModeratiors;
+	}
+	public void setChatModeratiors(ArrayList<String> chatModeratiors) {
+		this.chatModeratiors = chatModeratiors;
+	}
+	public ArrayList<String> getChatAdmins() {
+		return chatAdmins;
+	}
+	public void setChatAdmins(ArrayList<String> chatAdmins) {
+		this.chatAdmins = chatAdmins;
+	}
+	public String getChatHistory() {
+		return chatHistory;
+	}
 	
 	
 
